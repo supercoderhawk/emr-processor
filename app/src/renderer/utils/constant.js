e646ce721a812294d3e831c98b120ebce475d761
supercoderhawk <xyb930826@gmail.com>
2017-03-15 11:18:32 +0800

    modify constant in converter

--- app/src/renderer/utils/constant.js
+++ app/src/renderer/utils/constant.js
@@ -31,7 +31,6 @@ export const LIST = {
 export const CONVERTER = {
   BASE_PATH: '/api/v1/db/129/note/',
   BASE_URL: 'https://saas.synyi.com',
-  TEST_URL: 'https://saas.synyi.com/api/v1/db/129/note/14fe5761c6c540093e44bc4b',
   HOST: 'saas.synyi.com',
   INPUT_TYPES: [
     {val: 0, label: '一'},
