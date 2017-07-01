57f25799b4b52d4841364066b4cdcabb955eab6b
supercoderhawk <supercoderhawk@gmail.com>
2017-07-01 20:29:29 +0800

    add quill editor

---
+++ app/src/renderer/components/VueEditor.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="quillWrapper">
+        <div ref="quillContainer" :id="id"></div>
+    </div>
+</template>
+<script>
+  import Quill from 'quill'
+  import 'quill/dist/quill.core.css'
+  import 'quill/dist/quill.snow.css'
+
+  let defaultToolbar = [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block', 'image'],
+
+    [{'list': 'ordered'}, {'list': 'bullet'}],
+
+    [{'indent': '-1'}, {'indent': '+1'}],
+    [{'header': [1, 2, 3, 4, 5, 6, false]}],
+
+    [{'color': []}, {'background': []}],
+    [{'font': []}],
+    [{'align': []}],
+
+    ['clean']
+  ]
+
+  export default {
+    name: 'vue-editor',
+    props: {
+      value: String,
+      id: {
+        type: String,
+        'default': 'quill-container'
+      },
+      placeholder: String,
+      disabled: Boolean,
+      editorToolbar: Array
+    },
+
+    data () {
+      return {
+        quill: null,
+        editor: null,
+        toolbar: this.editorToolbar ? this.editorToolbar : defaultToolbar
+      }
+    },
+
+    mounted () {
+      this.initializeVue2Editor()
+      this.handleUpdatedEditor()
+    },
+
+    watch: {
+      value (val) {
+        if (val !== this.editor.innerHTML && !this.quill.hasFocus()) {
+          this.editor.innerHTML = val
+        }
+      },
+      disabled (status) {
+        this.quill.enable(!status)
+      }
+    },
+
+    methods: {
+      initializeVue2Editor () {
+        this.setQuillElement()
+        this.setEditorElement()
+        this.checkForInitialContent()
+      },
+
+      setQuillElement () {
+        this.quill = new Quill(this.$refs.quillContainer, {
+          modules: {
+            toolbar: false // this.toolbar
+          },
+          placeholder: this.placeholder ? this.placeholder : '',
+          theme: 'snow',
+          readOnly: this.disabled ? this.disabled : false
+        })
+        this.quill.on('text-change', (delta, oldDelta, source) => {
+          let text = this.quill.getText()
+          console.log(text)
+        })
+      },
+
+      setEditorElement () {
+        this.editor = document.querySelector(`#${this.id} .ql-editor`)
+      },
+
+      checkForInitialContent () {
+        this.editor.innerHTML = this.value || ''
+      },
+
+      handleUpdatedEditor () {
+        this.quill.on('text-change', () => {
+          this.$emit('input', this.editor.innerHTML)
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+    #quill-container {
+        min-height: 400px;
+    }
+</style>
