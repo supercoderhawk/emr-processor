b5a30ec8cfe8071667391f1c222e9e3237a979fb
supercoderhawk <supercoderhawk@gmail.com>
2017-07-03 20:53:13 +0800

    alter to quill

--- app/src/renderer/components/Entity.vue
+++ app/src/renderer/components/Entity.vue
@@ -1,90 +1,59 @@
 <template>
-    <div class="entity">
-        <div class="dropdown">
-            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="true">
-                下拉菜单按钮
-                <span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                <li class="dropdown-header">菜单头</li>
-                <li><a href="#">菜单1</a></li>
-                <li><a href="#">菜单2</a></li>
-                <li><a href="#">菜单3</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">菜单4</a></li>
-            </ul>
-        </div>
-        <div class="text-wrapper">
-            <bs-input ref="textarea" type="textarea" @dblclick.native="selectStart($event)"
-                      @select.native="select($event)" v-model="text" readonly></bs-input>
-        </div>
-        <div class="col-md-6 col-sm-6 col-xs-6 col-xs-lg-6">
-            <quill-editor v-model="content2"
-                          ref="myQuillEditor"
-                          :options="editorOption"
-                          @blur="onEditorBlur($event)"
-                          @focus="onEditorFocus($event)"
-                          @ready="onEditorReady($event)">
-            </quill-editor>
-            <vue-editor v-model="content"></vue-editor>
-        </div>
+  <div class="entity">
+    <!--<div class="dropdown">
+      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="true">
+          下拉菜单按钮
+          <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+        <li class="dropdown-header">菜单头</li>
+        <li><a href="#">菜单1</a></li>
+        <li><a href="#">菜单2</a></li>
+        <li><a href="#">菜单3</a></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="#">菜单4</a></li>
+      </ul>
+    </div>-->
+    <vue-editor v-model="content" ref="editor" @select="selectContent"></vue-editor>
 
-        <list></list>
-    </div>
+    <list></list>
+  </div>
 </template>
 
 <script>
-  // import Vue from 'vue'
-  import {input} from 'vue-strap'
   import list from './Entity/List'
-  // import VueQuillEditor from 'vue-quill-editor'
-  // Vue.use(VueQuillEditor)
-  import VueQuillEditor from 'vue-quill-editor/src/editor'
-  import {VueEditor} from 'vue2-editor'
+  import VueEditor from './VueEditor'
+  import {isRangeValid} from '../utils/utils'
 
   export default{
     components: {
-      'bs-input': input,
       'list': list,
-      VueEditor,
-      'quill-editor': VueQuillEditor
+      VueEditor
     },
     data () {
       return {
-        text: 'Texthhhhhhhhhhhhhhhhhhhhhhdfgdgfdfg',
-        content: 'Textaaaaa',
-        content2: 'This is a test',
-        editorOption: {}
+        content: 'Textaaaaa'
+      }
+    },
+    computed: {
+      editor () {
+        return this.$refs.editor.quill
       }
     },
     methods: {
-      select (event) {
-        // 获取选中数据
-        let start = event.target.selectionStart
-        let end = event.target.selectionEnd
-        let text = this.text.slice(start, end)
-        console.log(text)
-        this.$store.dispatch('addItem', {
-          id: start,
-          value: text,
-          type: 1,
-          start,
-          end
-        })
-      },
-      selectStart (event) {
-        console.log(this.$store)
-        return false
-      },
-      onEditorBlur (event) {
-
-      },
-      onEditorReady (event) {
-
-      },
-      onEditorFocus (event) {
-
+      selectContent (start, end, text) {
+        if (isRangeValid(start, end, this.$store.state.entity.items)) {
+          this.$store.dispatch('entity/addItem', {
+            id: start,
+            value: text,
+            type: 1,
+            start,
+            end
+          })
+          console.log(text)
+          this.editor.format('color', 'red')
+        }
       }
     },
     name: 'entity'
@@ -92,14 +61,9 @@
 </script>
 
 <style>
-    .entity textarea {
-        font-size: 24px;
-        width: 400px;
-    }
+  .entity textarea {
+    font-size: 24px;
+    width: 400px;
+  }
 
-    .ql-container {
-        min-height: 20em;
-        padding-bottom: 1em;
-        max-height: 25em;
-    }
 </style>
