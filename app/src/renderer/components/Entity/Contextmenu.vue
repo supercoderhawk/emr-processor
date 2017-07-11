90092b147b6e233098f662d27b79da5204922708
supercoderhawk <supercoderhawk@gmail.com>
2017-07-11 09:44:56 +0800

    add context menu component

---
+++ app/src/renderer/components/Entity/Contextmenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="dropdown-menu" :style="{left: left, top: top, display: this.display ? 'block' : 'none'}">
+    <li><a v-for="option in options" :key="option.val" @click="select(option.val)">{{option.label}}</a></li>
+  </ul>
+</template>
+
+<script>
+  import {ITEM} from '../../utils/constant'
+
+  export default {
+    methods: {
+      select (id) {
+        this.$emit('selectCompleted', id)
+      }
+    },
+    props: ['left', 'top', 'display'],
+    data () {
+      return {
+        options: ITEM.OPTIONS
+      }
+    },
+    name: 'contextmenu'
+  }
+</script>
+
+<style>
+
+</style>
