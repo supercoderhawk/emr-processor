374b53888c8d9fa41aa32571318e1090b94eeca9
supercoderhawk <supercoderhawk@gmail.com>
2017-07-17 08:44:07 +0800

    add remove item

--- app/src/renderer/store/modules/entity.js
+++ app/src/renderer/store/modules/entity.js
@@ -1,25 +1,35 @@
 import * as types from '../mutation-types'
 import {LIST} from '../../utils/constant'
-import {isRangeValid} from '../../utils/utils'
 
 const state = {
   items: LIST.INIT
 }
 
 const mutations = {
-  [types.ADD_ITEM] (state, item, index) {
-    state.items.splice(index, 0, item)
+  [types.ADD_ITEM] (state, item) {
+    state.items.splice(item.id, 0, item)
+  },
+  [types.REMOVE_ITEM] (state, item) {
+    state.items.splice(item.id, 1)
+  },
+  [types.RESORT_ITEM] (state) {
+    let items = state.items
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].id !== i) {
+        items[i].id = i
+      }
+    }
   }
 }
 
 const actions = {
   addItem: ({ commit, state }, item) => {
-    let items = state.items
-    if (isRangeValid(item.start, item.end, items)) {
-      let itemCount = items.length
-      let insertIndex = itemCount > 0 ? items.filter(curItem => { return curItem.end <= item.start }).length : 0
-      commit(types.ADD_ITEM, item, insertIndex)
-    }
+    commit(types.ADD_ITEM, item)
+    commit(types.RESORT_ITEM)
+  },
+  removeItem: ({commit, state}, item) => {
+    commit(types.REMOVE_ITEM, item)
+    commit(types.RESORT_ITEM)
   }
 }
 
